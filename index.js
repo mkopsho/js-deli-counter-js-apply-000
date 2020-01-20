855544d8cadb15705120fbd0cbad39b0d6772b13
Michael Kopsho <michael.kopsho@gmail.com>
2020-01-20 00:07:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function takeANumber(line, newPerson) {
   for (var i = 0; i < newPerson.length; i++) {
     line.push(newPerson);
   }
-  return `Welcome, ${newPerson}. You are number ${line[i]} in line.`;
+  return `Welcome, ${newPerson}. You are number ${line[i] + 1} in line.`;
 }
