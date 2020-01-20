9afecee9d7d61787f3516c4743468dc022e532f6
Michael Kopsho <michael.kopsho@gmail.com>
2020-01-20 00:49:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty.";
   }
   for (var i = 0; i < katzDeliLine.length; i++) {
-    currentLine.push(` ${i + 1}. ${katzDeliLine[i]}`);
+    currentLine.push(`${i + 1}. ${katzDeliLine[i]}`);
   }
   return `The line is currently: ${currentLine}.`;
 }
